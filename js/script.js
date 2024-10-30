f930d58fec908a9ad3a58e840d4b48f9080f22eb
Paulina Gajewska <gajewska.paulina08@gmail.com>
2024-10-30 06:54:31 +0100

    Change page content and style

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ function titleClickHandler(event){
   event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
-
+  const art0 = document.getElementById("article-0");
+  art0.classList.remove('active');
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
@@ -32,8 +33,42 @@ function titleClickHandler(event){
 
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post', 
+  optTitleSelector = '.post-title', 
+  optTitleListSelector = '.titles';
 
-for(let link of links){
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector); // lista artykułów
+  let html = '';
+
+  /* for each article */
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a class="links" href="#' + articleId + '"><span>' + articleTitle + '</span></a><hr></li>';
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+  }
+  
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+ 
+}
+
+generateTitleLinks();
